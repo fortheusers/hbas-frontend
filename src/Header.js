79c5f031ffed19e928eb563ff19e623f1d05181a
pwsincd <pwsincd@gmail.com>
2019-09-11 19:17:29 +0100

    Repo Select

--- src/Header.js
+++ src/Header.js
@@ -11,14 +11,18 @@ class Header extends Component {
       <div className="nav">
         <ul>
           <li id="title" className="title"><FontAwesomeIcon icon={faCube} />
-            &nbsp;&nbsp;Homebrew App Store <span className="platform">for {platform}</span>
+            &nbsp;&nbsp;Homebrew App Store <span className="platform">for </span>
+            <select id="device">
+              <option value="switch" selected>{platform}</option>
+              <option value="wiiu">WiiU</option>
+            </select>
           </li>
-        <li id="ftu"><a href="https://fortheusers.org">About</a></li>
-        <li id="discord"><a href="https://discord.gg/F2PKpEj">Discord</a></li>
-        <li id="twitter"><a href="https://twitter.com/wiiubru">Twitter</a></li>
-        <li id="account"><a href="https://www.switchbru.com/account/">Account</a></li>
-        <li id="dns"><a href="https://www.switchbru.com/dns">DNS</a></li>
-        <li id="submit"><a href="https://submit.fortheusers.org/">Submit</a></li>
+          <li id="ftu"><a href="https://fortheusers.org">About</a></li>
+          <li id="discord"><a href="https://discord.gg/F2PKpEj">Discord</a></li>
+          <li id="twitter"><a href="https://twitter.com/wiiubru">Twitter</a></li>
+          <li id="account"><a href="https://www.switchbru.com/account/">Account</a></li>
+          <li id="dns"><a href="https://www.switchbru.com/dns">DNS</a></li>
+          <li id="submit"><a href="https://submit.fortheusers.org/">Submit</a></li>
         </ul>
       </div >
     );
