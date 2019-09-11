7cd2aaa4fe7dfb15f9ced8af7c3dd2a030b9d96a
pwsincd <pwsincd@gmail.com>
2019-09-11 21:50:09 +0100

    Mobile Nav changes

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCube } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons'
+import { faCube, faPlus, faServer, faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import './MainDisplay.css';
 
 const platform = "Switch";
@@ -23,6 +24,12 @@ class Header extends Component {
           <li id="account"><a href="https://www.switchbru.com/account/">Account</a></li>
           <li id="dns"><a href="https://www.switchbru.com/dns">DNS</a></li>
           <li id="submit"><a href="https://submit.fortheusers.org/">Submit</a></li>
+          <li id="m_ftu"><a href="https://fortheusers.org"><FontAwesomeIcon icon={faCube} /></a></li>
+          <li id="m_discord"><a href="https://discord.gg/F2PKpEj"><FontAwesomeIcon icon={faDiscord} /></a></li>
+          <li id="m_twitter"><a href="https://twitter.com/wiiubru"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li id="m_account"><a href="https://www.switchbru.com/account/"><FontAwesomeIcon icon={faSignInAlt} /></a></li>
+          <li id="m_dns"><a href="https://www.switchbru.com/dns"><FontAwesomeIcon icon={faServer} /></a></li>
+          <li id="m_submit"><a href="https://submit.fortheusers.org/"><FontAwesomeIcon icon={faPlus} /></a></li>
         </ul>
       </div >
     );
