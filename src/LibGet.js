ed8bbc830b2a9484ef5b5396d1d34ca2d639a467
pwsincd <pwsincd@gmail.com>
2019-09-29 15:54:58 +0100

    ===

--- src/LibGet.js
+++ src/LibGet.js
@@ -2,24 +2,24 @@
 
 let repos = [];
 
-if (window.location.href == 'http://localhost:3000/') {
+if (window.location.href === 'http://localhost:3000/') {
  repos = [
   "https://4tu.gitlab.io/dragonite-test-repo",
 ];
 }
-else if (window.location.href == 'https://apps.fortheusers.org/switch')
+else if (window.location.href === 'https://apps.fortheusers.org/switch')
 {
   repos = [
     "https://www.switchbru.com/appstore/repo.json",
   ];
 }
-else if (window.location.href == 'https://apps.fortheusers.org/wiiu')
+else if (window.location.href === 'https://apps.fortheusers.org/wiiu')
 {
   repos = [
     "https://www.wiiubru.com/appstore/repo.json",
   ];
 }
-else if (window.location.href == 'https://apps.fortheusers.org/all')
+else if (window.location.href === 'https://apps.fortheusers.org/all')
 {
   repos = [
     "https://www.switchbru.com/appstore/repo.json",
