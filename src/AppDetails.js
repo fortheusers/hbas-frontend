7251e79242bf6370a4b726ef98eb04d9c9a877a0
pwsincd <pwsincd@gmail.com>
2019-10-11 15:27:39 +0100

    img alt error fix

--- src/AppDetails.js
+++ src/AppDetails.js
@@ -90,7 +90,7 @@ class AppDetails extends Component {
             </div>
             <div className="overlay">
             <img className="banner" src={`${repo}/packages/${name}/screen.png`} alt="banner" onError={e => { e.target.onerror=null; e.target.src=noscreen} } />
-            <img id="console" src={`${repo}/packages/logo.png`} />
+            <img id="console" src={`${repo}/packages/logo.png`} alt={platform} />
             </div>
           </div>
           <div className="right infoBox">
