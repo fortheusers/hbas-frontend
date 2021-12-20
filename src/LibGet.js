d0efe923d8f3bc5636138650eb3b3ba0ab8ae79e
pwsincd <pwsincd@gmail.com>
2021-12-20 15:52:49 +0000

    Update src/LibGet.js

--- src/LibGet.js
+++ src/LibGet.js
@@ -1,16 +1,16 @@
 const repos = [
     {
-      url: "http://switchbru.com/appstore",
+      url: "https://switchbru.com/appstore",
       platform: "switch"
     },
     {
       url: "https://wiiubru.com/appstore",
       platform: "wiiu"
-    }//,
-    //{
-    //  url: "https://3ds.apps.fortheusers.org",
-    //  platform : "3ds"
-    //}
+    },
+    {
+      url: "https://3ds.apps.fortheusers.org",
+      platform : "3ds"
+    }
     // {
     //   url: "https://4tu.gitlab.io/dragonite-test-repo",
     //   platform: "switch"
