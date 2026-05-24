030cb050cfed8ddeee4ee9af38d9a92b09be722a
etonedemid <3damir0n3@gmail.com>
2026-05-23 22:14:43 -0300

    Language detection/selection + russian and spanish locales

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import {init_theme, toggle_theme} from './Utils';
 import './MainDisplay.css';
 import { withTranslation } from 'react-i18next';
+import i18n from './i18n';
+
+const LANGUAGES = [
+  { code: 'en', label: 'English' },
+  { code: 'ru', label: 'Русский' },
+  { code: 'es', label: 'Español' },
+];
 
 class Footer extends Component {
-  // on mount, set the theme
   componentDidMount() {
     init_theme();
   }
@@ -19,6 +25,15 @@ class Footer extends Component {
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a className="copyright" href="/request-takedown">{t("dmca")}</a>
         </div>
         <div className="right">
+          <select
+            style={{fontSize: 10, padding: 4, marginRight: 8, verticalAlign: "top"}}
+            value={i18n.language}
+            onChange={e => i18n.changeLanguage(e.target.value)}
+          >
+            {LANGUAGES.map(({ code, label }) => (
+              <option key={code} value={code}>{label}</option>
+            ))}
+          </select>
           <button style={{fontSize: 10, padding: 4, marginRight: 15, verticalAlign: "top"}} onClick={toggle_theme}>
             {t("changeTheme")}
           </button>
--- src/i18n.jsx
+++ src/i18n.jsx
@@ -2,6 +2,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import enTranslation from './locales/en.json';
+import ruTranslation from './locales/ru.json';
+import esTranslation from './locales/es.json';
+
+const SUPPORTED = ['en', 'ru', 'es'];
+const detectedLang = (navigator.languages?.length ? navigator.languages : [navigator.language])
+  .map(l => l.split('-')[0])
+  .find(l => SUPPORTED.includes(l)) ?? 'en';
 
 i18n
   .use(initReactI18next)
@@ -9,9 +16,15 @@ i18n
     resources: {
       en: {
         translation: enTranslation
+      },
+      ru: {
+        translation: ruTranslation
+      },
+      es: {
+        translation: esTranslation
       }
     },
-    lng: 'en',
+    lng: detectedLang,
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false
